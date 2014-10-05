ddaa08fc5618d7394c1ceb1c57d2f173668ac579
peeedge <pj.harms@gmail.com>
2014-10-04 21:51:26 -0400

    Finished Class5 Lab2

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Bookstore::Application.routes.draw do
   resources :reviews
 
 resources :books do 
+  resources :reviews
   get 'page/:page', :action => :index, :on => :collection
 end
 root 'books#index'
